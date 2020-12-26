7f0bf0cee0d648d6e6e8adbd517c22b78d7dcf88
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-26 20:22:09 +0100

    Initial commit: Connecting and Inserting Documents

---
+++ mongodb.js
@@ -0,0 +1,23 @@
+// CRUD (create, read, update, delete)
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://pop-os.local:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log('Unable to connect to database');
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+      name: 'John Doe',
+      age: 30,
+    });
+  }
+);
