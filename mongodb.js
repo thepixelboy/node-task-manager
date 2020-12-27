7e776dc2f0b94d2bdb18225b00879bea23cbceac
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-27 14:58:21 +0100

    Querying Documents

--- mongodb.js
+++ mongodb.js
@@ -5,10 +5,6 @@ const { MongoClient, ObjectID } = require('mongodb');
 const connectionURL = 'mongodb://pop-os.local:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectID();
-console.log(id.id.length);
-console.log(id.toHexString().length);
-
 MongoClient.connect(
   connectionURL,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -19,57 +15,56 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne(
-    //   {
-    //     name: 'Jane Doe',
-    //     age: 28,
-    //   },
-    //   (error, result) => {
+    // db.collection('users').findOne(
+    //   { _id: new ObjectID('5fe88b93b2306dd5f84680a1') },
+    //   (error, user) => {
     //     if (error) {
-    //       return console.log('Unable to insert user');
+    //       return console.log('Unable to fetch document');
     //     }
 
-    //     console.log(result.ops);
+    //     console.log(user);
     //   }
     // );
-    // db.collection('users').insertMany(
-    //   [
-    //     {
-    //       name: 'Jen',
-    //       age: 28,
-    //     },
-    //     { name: 'Gunter', age: 27 },
-    //   ],
-    //   (error, result) => {
+
+    // db.collection('users')
+    //   .find({ age: 30 })
+    //   .toArray((error, users) => {
     //     if (error) {
-    //       return console.log('Unable to insert documents');
+    //       return console.log('Unable to fetch documents');
     //     }
 
-    //     console.log(result.ops);
-    //   }
-    // );
-    // db.collection('tasks').insertMany(
-    //   [
-    //     {
-    //       description: 'Complete Node.js lesson 73',
-    //       completed: true,
-    //     },
-    //     {
-    //       description: 'Complete Node.js lesson 74',
-    //       completed: true,
-    //     },
-    //     {
-    //       description: 'Complete Node.js lesson 77',
-    //       completed: false,
-    //     },
-    //   ],
-    //   (error, result) => {
+    //     console.log(users);
+    //   });
+
+    // db.collection('users')
+    //   .find({ age: 30 })
+    //   .count((error, count) => {
     //     if (error) {
-    //       return console.log('Unable to insert documents');
+    //       return console.log('Unable to fetch documents');
     //     }
 
-    //     console.log(result.ops);
-    //   }
-    // );
+    //     console.log(count);
+    //   });
+
+    db.collection('tasks').findOne(
+      { _id: new ObjectID('5fe887785dd178d4952d2a81') },
+      (error, task) => {
+        if (error) {
+          return console.log('Unable to fetch document');
+        }
+
+        console.log(task);
+      }
+    );
+
+    db.collection('tasks')
+      .find({ completed: false })
+      .toArray((error, tasks) => {
+        if (error) {
+          return console.log('Unable to fetch documents');
+        }
+
+        console.log(tasks);
+      });
   }
 );
