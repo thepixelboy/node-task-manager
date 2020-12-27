caba1945f5bb9a6b2ad2a1ca123e890c1d8bb34f
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-27 14:15:05 +0100

    Inserting Documents

--- mongodb.js
+++ mongodb.js
@@ -15,9 +15,57 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-      name: 'John Doe',
-      age: 30,
-    });
+    // db.collection('users').insertOne(
+    //   {
+    //     name: 'John Doe',
+    //     age: 30,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert user');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+    // db.collection('users').insertMany(
+    //   [
+    //     {
+    //       name: 'Jen',
+    //       age: 28,
+    //     },
+    //     { name: 'Gunter', age: 27 },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert documents');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+    db.collection('tasks').insertMany(
+      [
+        {
+          description: 'Complete Node.js lesson 73',
+          completed: true,
+        },
+        {
+          description: 'Complete Node.js lesson 74',
+          completed: true,
+        },
+        {
+          description: 'Complete Node.js lesson 77',
+          completed: false,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert documents');
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
