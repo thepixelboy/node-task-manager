a18924378ffc744963e0251df0982533f115a798
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-29 12:57:11 +0100

    Resource Creation Endpoints: Part II

--- src/index.js
+++ src/index.js
@@ -14,7 +14,20 @@ app.post('/users', (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
     })
     .catch((err) => {
       res.status(400).send(err);
--- src/models/task.js
+++ src/models/task.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+// const validator = require('validator');
 
 const Task = mongoose.model('Task', {
   description: {
