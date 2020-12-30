418ab981989601c45a3c039e26d9fc4b59412251
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-30 13:41:29 +0100

    Securely Storing Passwords: Part II

--- src/routers/user.js
+++ src/routers/user.js
@@ -48,10 +48,18 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true,
+    // });
+    //
+    // The precedent is replaced with the next 3 lines in order to use middleware
+    // when saving and updating the user.
+
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
 
     if (!user) {
       return res.status(404).send({ error: 'User not found' });
