0e77f34ca407182aee232c1ad11ee24d48a0f98a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-01-01 18:19:44 +0100

    Express Middleware

--- src/index.js
+++ src/index.js
@@ -10,6 +10,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 const jwtSecret = process.env.JWT_SECRET_KEY;
 
+// app.use((req, res, next) => {
+//   if (req.method === 'GET') {
+//     res.send('GET requests are disabled');
+//   } else {
+//     next();
+//   }
+// });
+
+app.use((req, res, next) => {
+  res.status(503).send({
+    message: 'Maintenance mode. Please return back in a few minutes. Thanks',
+  });
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
