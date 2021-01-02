3d0223cf0c256f8b318fbbe9ca02ff5774998a70
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-01-02 18:23:54 +0100

    Hiding Private Data

--- src/index.js
+++ src/index.js
@@ -31,18 +31,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// Testing bcryptjs
-// const jwt = require('jsonwebtoken');
-
-// const myFunction = async () => {
-//   const token = jwt.sign({ _id: 'abc123' }, jwtSecret, {
-//     expiresIn: '7 days',
-//   });
-//   console.log(token);
-
-//   const data = jwt.verify(token, jwtSecret);
-//   console.log(data);
-// };
-
-// myFunction();
