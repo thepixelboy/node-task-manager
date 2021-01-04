9423383f3eeefbf5e8e1321a9305d62c0d43922d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-01-04 19:40:49 +0100

    Auto-Cropping and Image Formatting

--- src/index.js
+++ src/index.js
@@ -9,33 +9,6 @@ dotenv.config({ path: './config.env' });
 const app = express();
 const port = process.env.PORT || 3000;
 
-const multer = require('multer');
-const upload = multer({
-  dest: 'images',
-  limits: {
-    fileSize: 1000000,
-  },
-  fileFilter(req, file, cb) {
-    // cb(new Error('File must be a PDF'));
-    // cb(undefined, true);
-    // cb(undefined, false);
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error('Only Word files (DOC, DOCX) are allowed'));
-    }
-    cb(undefined, true);
-  },
-});
-app.post(
-  '/upload',
-  upload.single('upload'),
-  (req, res) => {
-    res.send();
-  },
-  (err, req, res, next) => {
-    res.status(400).send({ error: err.message });
-  }
-);
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
