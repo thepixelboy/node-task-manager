c6dd87db850695f6955908af87c163e548e86d87
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-01-05 18:04:28 +0100

    Writing Your Own Tests

--- src/math.js
+++ src/math.js
@@ -1,3 +1,11 @@
 const calculateTip = (total, tipPercent = 0.25) => total + total * tipPercent;
 
-module.exports = { calculateTip };
+const celsiusToFahrenheit = (temp) => {
+  return temp * 1.8 + 32;
+};
+
+const fahrenheitToCelsius = (temp) => {
+  return (temp - 32) / 1.8;
+};
+
+module.exports = { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius };
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,8 @@
-const { calculateTip } = require('../src/math');
+const {
+  calculateTip,
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+} = require('../src/math');
 
 test('Should calculate total with tip', () => {
   const total = calculateTip(10, 0.3);
@@ -8,4 +12,14 @@ test('Should calculate total with tip', () => {
 test('Should calculate total with default tip', () => {
   const total = calculateTip(10);
   expect(total).toBe(12.5);
+});
+
+test('Should convert 32 F to 0 C', () => {
+  const temperature = fahrenheitToCelsius(32);
+  expect(temperature).toBe(0);
+});
+
+test('Should convert 0 C to 32 F', () => {
+  const temperature = celsiusToFahrenheit(0);
+  expect(temperature).toBe(32);
 });
