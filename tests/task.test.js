170752812ebcdd577326b2b7e382d997c28429b4
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-01-07 21:21:07 +0100

    Testing with Task Data

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,17 @@
+const { send } = require('@sendgrid/mail');
 const request = require('supertest');
 const app = require('../src/app');
 const Task = require('../src/models/task');
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
+const {
+  userOneId,
+  userOne,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
+  setupDatabase,
+} = require('./fixtures/db');
 
 beforeEach(setupDatabase);
 
@@ -17,4 +27,25 @@ test('Should create a task for user', async () => {
   const task = await Task.findById(response.body._id);
   expect(task).not.toBeNull();
   expect(task.completed).toBe(false);
+});
+
+test('Should get all tasks for user', async () => {
+  const response = await request(app)
+    .get('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+});
+
+test('Should not delete other users tasks', async () => {
+  const response = await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  const task = await Task.findById(taskOne._id);
+  expect(task).not.toBeNull();
 });
